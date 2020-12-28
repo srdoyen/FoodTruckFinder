afd2123cf8819360c57bd8b6c0591c57df7488d8
Scott Doyen <srdoyen@gmail.com>
2020-12-27 18:08:10 -0800

    add sample back

---
+++ sample.js
@@ -0,0 +1,8 @@
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBekCwoIih7wLZgbhtRVFw_4Dbmn7QbFAg';
+script.defer = true;
+
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
